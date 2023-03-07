680031e0fae4c54e92d68e23e0d4669af24d2d53
Prithwis Das <prithwisdas2017@gmail.com>
2023-03-06 19:30:55 -0800

    update

--- map.py
+++ map.py
@@ -5,14 +5,14 @@ from scipy import ndimage
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-'''
+
 fig.set_facecolor('black')
 ax.set_facecolor('black') 
-ax.grid(True) 
+ax.grid(False) 
 ax.w_xaxis.pane.fill = False
 ax.w_yaxis.pane.fill = False
 ax.w_zaxis.pane.fill = False
-'''
+
 
 # points for Sun to Galactic Center
 z_galc = np.repeat(0, 1000)
@@ -37,14 +37,14 @@ x_6441 = np.repeat(0, 1000)
 y_6441 = -0.08766453505988449896723384277002*z_6441 #for B
 x_6441 = -0.11340524411253067779382016726172*y_6441 #for L
 
-ax.set_xlabel('X-Axis', c='black')
-ax.set_ylabel('Y-Axis', c='black')
-ax.set_zlabel('Z-Axis', c='black')
+ax.set_xlabel('X-Axis', c='white')
+ax.set_ylabel('Y-Axis', c='white')
+ax.set_zlabel('Z-Axis', c='white')
 
 # plot
-ax.plot(x_galc, y_galc, z_galc, label=r'Sun to Gal. Center')
-ax.plot(x_tuc, y_tuc, z_tuc, label=r'Sun to 47 Tucanae')
-ax.plot(x_5139, y_5139, z_5139, label=r'Sun to NGC 5139')
-ax.plot(x_6441, y_6441, z_6441, label=r'Sun to NGC 6441')
+ax.plot(x_galc, y_galc, z_galc, c='white', label=r'Sun to Gal. Center')
+ax.plot(x_tuc, y_tuc, z_tuc, c='white', label=r'Sun to 47 Tucanae')
+ax.plot(x_5139, y_5139, z_5139,c='white', label=r'Sun to NGC 5139')
+ax.plot(x_6441, y_6441, z_6441, c='yellow', label=r'Sun to NGC 6441')
 plt.legend()
 plt.show()
