9faf43941b043c80ffe2eea57377034413d0e004
Prithwis Das <prithwisdas2017@gmail.com>
2023-03-09 19:18:04 -0800

    update

--- map.py
+++ map.py
@@ -7,16 +7,13 @@ import pandas as pd
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-'''
+
 fig.set_facecolor('black')
 ax.set_facecolor('black') 
 ax.grid(False) 
 ax.w_xaxis.pane.fill = False
 ax.w_yaxis.pane.fill = False
 ax.w_zaxis.pane.fill = False
-'''
-data = pd.read_csv('Harris.csv')
-
 
 def plot_gc(ID, l, b, dist_Sun, points):
     z_coor = np.linspace(0, dist_Sun, num = points)
@@ -25,7 +22,6 @@ def plot_gc(ID, l, b, dist_Sun, points):
     x_coor = math.tan(l*(math.pi/180)) * y_coor #first converted to radians
     ax.plot(x_coor, y_coor, z_coor, label = r'Sun to NGC {}'.format(ID))
         
-
 # points for Sun to Galactic Center
 z_galc = np.repeat(0, 1000)
 x_galc = np.repeat(0, 1000) 
@@ -42,9 +38,11 @@ plot_gc(7089, 53.37, -35.77, -11.5, 1000) # NGC 7089
 plot_gc(7078, 65.01, -27.31, -10.4, 1000) # NGC 7078
 plot_gc(1851, 244.51, -35.03, 12.1, 1000) # NGC 1851
 
-ax.set_xlabel('X-Axis', c='black')
-ax.set_ylabel('Y-Axis', c='black')
-ax.set_zlabel('Z-Axis', c='black')
+color = 'white' # specifying color for label of axes
+
+ax.set_xlabel('X-Axis', c=color)
+ax.set_ylabel('Y-Axis', c=color)
+ax.set_zlabel('Z-Axis', c=color)
 
 plt.legend()
 plt.show()
