2c61a1571e5e91a458770a02ff60c5d7589d7c8c
Prithwis Das <prithwisdas2017@gmail.com>
2023-11-11 21:43:37 -0800

    update

--- map.py
+++ map.py
@@ -18,9 +18,12 @@ ax.w_zaxis.pane.fill = False
 
 def plot_gc(ID, l, b, dist_Sun, points):
     z_coor = np.linspace(0, dist_Sun, num = points)
-    x_coor = np.repeat(0, 1000)
-    y_coor = math.tan(b*(math.pi/180)) * z_coor #first converted to radians 
-    x_coor = math.tan(l*(math.pi/180)) * y_coor #first converted to radians
+    #x_coor = np.repeat(0, points)
+    x_coor =  math.cos(b * (math.pi / 180)) * math.sin(l * (math.pi / 180)) * abs(z_coor)
+    y_coor = np.repeat(0, points)
+    y_coor = math.cos(b * (math.pi / 180)) * math.cos(l * (math.pi / 180)) * abs(z_coor)
+    #y_coor = math.tan(b*(math.pi/180)) * z_coor #first converted to radians 
+    #x_coor = math.tan(l*(math.pi/180)) * y_coor #first converted to radians
     ax.plot(x_coor, y_coor, z_coor, label = r'Sun to NGC {}'.format(ID), linewidth=2)
         
 # points for Sun to Galactic Center
@@ -31,13 +34,13 @@ y_galc = np.linspace(start=0.0, stop=8.0, num=1000)
 ax.plot(x_galc, y_galc, z_galc, label=r'Sun to Gal. Center', linewidth=2)
 
 plot_gc(104, 305.89, -44.89, -4.5, 1000)  # NGC 104 (47 Tucanae)
-plot_gc(5139, 309.10, -14.97, -5.2, 1000) # NGC 5139 (Omega Centauri)
+plot_gc(5139, 309.10, 14.97, 5.2, 1000) # NGC 5139 (Omega Centauri)
 plot_gc(6441, 353.53, -5.01, -11.6, 1000) # NGC 6441
 plot_gc(6440, 7.73, 3.80, 8.5, 1000)      # NGC 6440 
 plot_gc(6402, 21.32, 14.81, 9.3, 1000)    # NGC 6402
 plot_gc(7089, 53.37, -35.77, -11.5, 1000) # NGC 7089
 plot_gc(7078, 65.01, -27.31, -10.4, 1000) # NGC 7078
-plot_gc(1851, 244.51, -35.03, 12.1, 1000) # NGC 1851
+plot_gc(1851, 244.51, -35.03, -12.1, 1000) # NGC 1851
 
 color = 'white' # specifying color for label of axes
 
@@ -46,6 +49,7 @@ ax.set_ylabel('Y-Axis', c=color)
 ax.set_zlabel('Z-Axis', c=color)
 
 plt.legend()
+plt.savefig('3d-map.png', dpi = 1000)
 plt.show()
 
 
